a86c634c5ee709132aeea6a137a452ad7cc23b76
Hernán Sartorio <hernan124@gmail.com>
2019-12-15 14:33:12 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './style.scss'
+
+function App() {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
